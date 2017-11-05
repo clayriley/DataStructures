a327e8ad6e1e39b0d8ecab0369111d3cc4bcfc82
clayriley <riley@brandeis.edu>
2017-11-05 10:20:50 -0500

    splaying and rotation operations completed

--- splayTree.py
+++ splayTree.py
@@ -39,38 +39,89 @@ class SplayTree():
         pass
 
     def _splay(self):
+        """
+        Splay the tree around the node until it is the root.  
+        O(n), amortized O(log n)
+        """
 
-        # case 1: already the root -> do nothing
-        if self.parent is not None:
+        if self.parent is not None: # case 1: already the root -> do nothing
             grandparent = self.parent.parent
 
-            # case 2: one rotation to root
-            if grandparent is None:
+            if grandparent is None: # case 2: one rotation to root
                 self._zig()
 
             else: # case 3: multiple rotations to root
                 rightOfLeft = self.parent.right is self and grandparent.left is self.parent
                 leftOfRight = self.parent.left is self and grandparent.right is self.parent
 
-                # case 3a: zigzag
-                if rightOfLeft or leftOfRight:
+                if rightOfLeft or leftOfRight: # case 3a: zigzag
                     self.zig()
                     self.zig()
 
-                # case 3b: zigzig
-                else:
+                else: # case 3b: zigzig
                     self.parent.zig()
                     self.zig()
-                self._splay() # recur
+
+                self._splay() # recur until case 1 or 2 applies
     
     def _zig(self):
-        pass
+        """
+        Perform one zig (or zag) operation to rotate the node upward.
+        O(1)
+        """
+        if self.parent is not None:
+            if self is self.parent.left:
+                self._rotateClockwise()
+            elif self is self.parent.right:
+                self._rotateCounterClockwise()
 
     def _rotateClockwise(self):
-        pass
+        """
+        Perform one AVL-style rotation clockwise.  Requires a rightward parent.
+        O(1)
+        """
+        p, g, r = self.parent, self.parent.parent, self.right
+
+        # connect right child to parent
+        if r is not None:
+            r.parent = p
+        p.left = r
+        
+        # move parent down + right
+        p.parent = self
+        self.right = p
+
+        # move this node up + right
+        self.parent = g
+        if g is not None:
+            if p is g.left:
+                g.left = self
+            elif p is g.right:
+                g.right = self
     
     def _rotateCounterclockwise(self):
-        pass
+        """
+        Perform one AVL-style rotation counterclockwise.  Requires a leftward parent.
+        O(1)
+        """
+        p, g, l = self.parent, self.parent.parent, self.left
+
+        # connect left child to parent
+        if l is not None:
+            l.parent = p
+        p.right = l
+        
+        # move parent down + left
+        p.parent = self
+        self.left = p
+
+        # move this node up + left
+        self.parent = g
+        if g is not None:
+            if p is g.left:
+                g.left = self
+            elif p is g.right:
+                g.right = self
 
     def _uproot(self):
         pass
