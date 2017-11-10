3270c5ebc441ec2210067509872b2b354d141c77
clayriley <riley@brandeis.edu>
2017-11-10 18:51:29 -0500

    added wrapper and functions for typed lists

--- myList.py
+++ myList.py
@@ -39,10 +39,47 @@ class List():
 
 class TypedList():
     
-    def __init__(self, t=None):
-        if t is not None:
-            self.t = t
+    def __init__(self, typing=None):
+        self.typing = typing # if None, lazy (typed upon first insertion)
         self.container = []
+        self._size = 0
 
-    def insert(self, element):
-        pass
+    @property
+    def size(self):
+        return self._size # could also just point to len(self.container)
+
+    def insert(self, element, head=False):
+
+        if self.typing is None: # first insertion: set type
+            self.typing = type(element)
+        else: # check type
+            if type(element) != self.typing:
+                raise TypeError("Type " + str(type(element)) + " is " + 
+                                "incompatible with list of type " + 
+                                str(self.typing) + ".")
+        # if no error:
+        if head:
+            self.container.insert(0, element)
+        else:
+            self.container.append(element)
+        self._size += 1
+
+    def remove(self, element):
+        if type(element) == self.typing: # don't waste time if bad type
+            for e,i in enumerate(self):
+                if e == element:
+                    self.container = self.container[:i] + self.container[i+1:]
+                    self._size -= 1
+                    break
+
+    def __iter__(self):
+        return (item for item in self.container)
+
+    def __contains__(self, element):
+        if type(element) != self.typing:
+            return False # faster return with bad types
+        else:
+            return element in self.container
+    
+    def __str__(self):
+        return "<" + ", ".join(str(x) for x in self.container) + ">"
